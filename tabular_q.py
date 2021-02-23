14948057dd8ea46b59a7007bfc0622e315032d30
Frederico Nesti <fredericonesti@gmail.com>
2021-02-23 11:13:11 +0100

    bac

--- tabular_q.py
+++ tabular_q.py
@@ -22,8 +22,10 @@ def default_output_dir() -> str:
 
 # Program entrypoint
 if __name__ == '__main__':
+    print('ok')
     random_seed = 0
     util.create_artefact_dirs(default_output_dir(), random_seed)
+    print('ok')
     q_agent_config = QAgentConfig(gamma=0.999, alpha=0.0005, epsilon=1, render=False, eval_sleep=0.9,
                                   min_epsilon=0.01, eval_episodes=100, train_log_frequency=100,
                                   epsilon_decay=0.9999, video=True, eval_log_frequency=1,
@@ -31,9 +33,13 @@ if __name__ == '__main__':
                                   eval_render=False, gifs=True, gif_dir=default_output_dir() + "/results/gifs/" + str(random_seed),
                                   eval_frequency=1000, attacker=True, defender=False, video_frequency=101,
                                   save_dir=default_output_dir() + "/results/data/" + str(random_seed))
+    print('ok')
     env_name = "idsgame-minimal_defense-v2"
     env = gym.make(env_name, save_dir=default_output_dir() + "/results/data/" + str(random_seed))
+    print('ok')
     attacker_agent = TabularQAgent(env, q_agent_config)
     attacker_agent.train()
+    print('ok')
     train_result = attacker_agent.train_result
     eval_result = attacker_agent.eval_result
+    print('ok')
